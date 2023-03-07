46054d7e8d41c6667063d3d810644faa6b93c79a
alisa-qatipi <121465168+alisa-qatipi@users.noreply.github.com>
2023-03-07 10:01:33 +0100

    CSS added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
      <div className="container-fluid">
       <div className="container">
         <div className="buttonContainer d-flex justify-content-end mt-3">
-        <button className="btn btn-secondary" onClick={()=>setOpenModal(true)}>show popup</button>
+        <button className="button" onClick={()=>setOpenModal(true)}>show popup</button>
       {/* it means that if the openModal is true it will display the modal component */}
       {openModal && <Modal closeModal={setOpenModal}/>}
         </div>
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -5,44 +5,44 @@ function Modal({closeModal}) {
   return (
     <>
        <div className="modalBackground" onClick={() => closeModal(false)} style={{color: "#444444"}}>
-        <div className="modalContainer mt-4 pt-3 ps-5 pe-5 w-50">
+        <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
             {/* <button className='btn btn-outline-dark' onClick={() => closeModal(false)}>X</button> */}
-            <div className="title d-flex justify-content-between mt-2 mb-5">
+            <div className="title">
                 <h5>Theme Color</h5>
                 <h6 className='text-primary'>Change Theme</h6>
             </div>
             <div className="body">
-                <div className="fontColor d-flex justify-content-between border-bottom">
+                <div className="row">
                     <p>Font Color</p>
-                    <div className="fontColorContent d-flex gap-2">
+                    <div className="color fontColorContent d-flex gap-2">
                         <p>#444444</p>
                         <input type="color" name="fontColorInput" id="fontColorInput" value="#444444"/>
                     </div>
                 </div>
-                <div className="backgroundColor d-flex justify-content-between border-bottom mt-3">
+                <div className="row backgroundColor d-flex justify-content-between border-bottom mt-3">
                     <p>Background Color</p>
-                    <div className="backgroundColorContent d-flex gap-2">
+                    <div className="color backgroundColorContent d-flex gap-2">
                         <p>#FFFFFF</p>
                         <input type="color" name="backgroundColorInput" id="backgroundColorInput" value="#FFFFFF"/>
                     </div>
                 </div>
-                <div className="buttonColor d-flex justify-content-between border-bottom mt-3">
+                <div className="row buttonColor d-flex justify-content-between border-bottom mt-3">
                     <p>Button Color</p>
-                    <div className="buttonColorContent d-flex gap-2">
+                    <div className="color buttonColorContent d-flex gap-2">
                         <p>#2072EF</p>
                         <input type="color" name="buttonColorInput" id="buttonColorInput" value="#2072EF"/>
                     </div>
                 </div>
-                <div className="buttonBorderColor d-flex justify-content-between border-bottom mt-3">
+                <div className="row buttonBorderColor d-flex justify-content-between border-bottom mt-3">
                     <p>Button Border Color</p>
-                    <div className="buttonBorderColorContent d-flex gap-2">
+                    <div className="color buttonBorderColorContent d-flex gap-2">
                         <p>#2072EF</p>
                         <input type="color" name="buttonBorderColorInput" id="buttonBorderColorInput" value="#2072EF"/>
                     </div>
                 </div>
-                <div className="buttonMouseoverColor d-flex justify-content-between border-bottom mt-3">
+                <div className="row buttonMouseoverColor d-flex justify-content-between border-bottom mt-3">
                     <p>Button Mouseover Color</p>
-                    <div className="buttonMouseoverColorContent d-flex gap-2">
+                    <div className="color buttonMouseoverColorContent d-flex gap-2">
                         <p>#0053D1</p>
                         <input type="color" name="buttonMouseoverColorInput" id="buttonMouseoverColorInput" value="#0053D1"/>
                     </div>
